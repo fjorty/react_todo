519d25f761102f4248d3f131c4cebf88b1221e99
fjorty <naz.lavryk@gmail.com>
2019-03-17 00:39:36 +0200

    add homepage

---
+++ build/precache-manifest.9ad1508f4a5522a9b9fe7b08caab9cbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d938fe3b64b5cffa180f",
+    "url": "/static/js/main.d88f10a2.chunk.js"
+  },
+  {
+    "revision": "d12ce5744feac965fafb",
+    "url": "/static/js/2.bd37510c.chunk.js"
+  },
+  {
+    "revision": "d938fe3b64b5cffa180f",
+    "url": "/static/css/main.52c8e9be.chunk.css"
+  },
+  {
+    "revision": "647347ed4df413f034ea86cecaa08821",
+    "url": "/index.html"
+  }
+];
